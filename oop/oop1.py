a04ca2520deefc5b401d8c85e2c1546142dadefd
kamaukimani <paulkimys285@gmail.com>
2026-07-23 14:35:27 +0300

    learn the self keyword and the dunder method

--- oop/oop1.py
+++ oop/oop1.py
@@ -8,10 +8,25 @@ fido=Dog()
 fido.bark()
 fido.sit()
 class Person:
+    def __init__(self,name):
+        self.name=name
+        print(f"Hello {name}")
     def talk(self):
-        print("Hello world!!")
+        print("Hello World!")
     def walk(self):
         print("The person is walking")
-john=Person()
+john=Person("John")
+print(john.name)
 john.talk()
 john.walk()
+class Dog1:
+    def __init__(self,name,breed="Mutt"):
+        self.name=name
+        self.breed=breed
+        print(f"{name} is born!")
+    def bark(self):
+        print("Woof!!")
+fido1=Dog1("Fido")
+print(fido1.name)
+fido1.bark()
+print(fido1.breed)
