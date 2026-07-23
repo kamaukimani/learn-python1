4c802d11a448455fee6756e9ba7da19e9d833efc
kamaukimani <paulkimys285@gmail.com>
2026-07-23 09:44:24 +0300

    update file

--- oop/oop1.py
+++ oop/oop1.py
@@ -1,1 +1,17 @@
 #learn object oriented programming
+class Dog:
+    def bark(self):
+        print("Whooaf!!!")
+    def sit(self):
+        print("The dog is sitting")
+fido=Dog()
+fido.bark()
+fido.sit()
+class Person:
+    def talk(self):
+        print("Hello world!!")
+    def walk(self):
+        print("The person is walking")
+john=Person()
+john.talk()
+john.walk()
--- solution.py
+++ solution.py
@@ -111,3 +111,51 @@ def calculator(operation, num1, num2):
 
     print("Invalid operation!")
     return None
+
+
+    spicy_foods = [
+    {
+        "name": "Green Curry",
+        "cuisine": "Thai",
+        "heat_level": 9,
+    },
+    {
+        "name": "Buffalo Wings",
+        "cuisine": "American",
+        "heat_level": 3,
+    },
+    {
+        "name": "Mapo Tofu",
+        "cuisine": "Sichuan",
+        "heat_level": 6,
+    },
+]
+
+def get_names(spicy_foods):
+    return [food["name"] for food in spicy_foods]
+
+def get_spiciest_foods(spicy_foods):
+    return [food for food in spicy_foods if food["heat_level"] > 5]
+
+def print_spicy_foods(spicy_foods):
+    for food in spicy_foods:
+        print(f'{food["name"]} ({food["cuisine"]}) | Heat Level: {"🌶" * food["heat_level"]}')
+
+def create_spicy_food(spicy_foods, spicy_food):
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine):
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            return food
+
+# def sort_by_heat(spicy_foods):
+#     return sorted(spicy_foods, key=lambda h: h['heat_level'])
+
+def print_spiciest_foods(spicy_foods):
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
+
+def get_average_heat_level(spicy_foods):
+    return sum([food["heat_level"] for food in spicy_foods]) / len(spicy_foods)
