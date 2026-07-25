9eab09b8ef5a10a30dcf61a99291b357f3715c34
kamaukimani <paulkimys285@gmail.com>
2026-07-25 14:08:53 +0300

    use the property() function to set getters and setters

--- oop/property.py
+++ oop/property.py
@@ -26,3 +26,43 @@ setattr(guido,"age",30)
 print(guido.age)
 delattr(guido,"age") #deletes the attribute from the object
 #print(guido.age)
+class Human1:
+    species="Homo Sapien Sapiens"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def get_age(self):
+        print(f"Retrieving age")
+        return self._age
+    def set_age(self,age):
+        print(f"Setting age to {age}")
+        self._age=age
+    age=property(get_age,set_age)
+guido1=Human1("Guido",30)
+print(guido1.age)
+guido.age=42
+print(guido.age)
+guido1.get_age()
+guido1.set_age(54)
+class Human2:
+    species="Homo Erectus"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def get_age(self):
+        print("Retrieving the age")
+        return self._age
+    def set_age(self,age):
+        if(type(age)in(int,float)) and (0<=age<=120):
+            print(f"Setting age to {age}")
+            self._age=age 
+        else:
+            print(f"{age} must be a number between 0 and 120")
+    age=property(get_age,set_age)
+guido2=Human2(name="Guido",age=66)
+guido2.get_age()
+guido2.age="Alice"
+guido2_age=guido2.get_age()
+print(guido2_age)
+setattr(guido2,"age",122)
+
