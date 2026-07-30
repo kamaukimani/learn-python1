be7c3548d0eab5b3c49ce3ab6b7535a889e8792f
kamaukimani <paulkimys285@gmail.com>
2026-07-30 14:14:12 +0300

    code the Anagram class

--- oop/anagram.py
+++ oop/anagram.py
@@ -1,1 +1,16 @@
 print("Hello!!!Welcome to the Anagram class")
+#print(sorted("hello"))
+class Anagram:
+    def __init__(self,word):
+        self.word=word
+    def match(self,word_list):
+        matches=[]
+        
+        for word in word_list:
+            if sorted(word)==sorted(self.word):
+                print(f"commparing: {word} with: {self.word}")
+                matches.append(word)
+        return matches
+anagram=Anagram("enlist")
+matching=anagram.match(["hello","listen","come","stenli","setnil"])
+print(matching)
