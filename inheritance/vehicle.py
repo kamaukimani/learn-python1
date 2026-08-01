1c621e7466f8cafea49c976b9fab11cb8c76fb6b
kamaukimani <paulkimys285@gmail.com>
2026-08-01 12:54:35 +0300

    code the Parent class

--- inheritance/vehicle.py
+++ inheritance/vehicle.py
@@ -1,1 +1,10 @@
-print("Test file")
+print("Test Vehicle classe")
+class Vehicle:
+    def __init__(self,wheel_size,wheel_number):
+        self.wheel_size=wheel_size
+        self.wheel_number=wheel_number
+    def go(self):
+        return "Vrooom"
+    def fill_up_tank(self):
+        return "Filling up!"
+    
