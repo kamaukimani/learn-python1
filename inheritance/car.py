35a05d5e3e06f950b96d16300a4b8348ce88be78
kamaukimani <paulkimys285@gmail.com>
2026-08-01 12:55:14 +0300

    code the child class

--- inheritance/car.py
+++ inheritance/car.py
@@ -1,1 +1,9 @@
+from vehicle import Vehicle
 print("Welcome.....Let's code the Car class")
+class Car(Vehicle):
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOM!!!!!!!!!!!!"
+
+car=Car("Ten",34)
+print(car.go())
+print(car.fill_up_tank())
