ba7eaca8c9c5a12813313f05f5f097d81929cb19
kamaukimani <paulkimys285@gmail.com>
2026-08-09 12:28:14 +0300

    two way one-to-many relationship using association

--- relationship/one_many.py
+++ relationship/one_many.py
@@ -1,5 +1,6 @@
 print("Relationships in objects")
 #association
+#one way
 class GroceryItem:
     def __init__(self,name,price):
         self.name=name
@@ -16,3 +17,37 @@ shopper.grocery_items.append(item1)
 shopper.grocery_items.append(item2)
 for item in shopper.grocery_items:
     print(item.name,item.price)
+
+# two way
+class Student:
+    all=[]
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+        self._teacher=None
+        Student.all.append(self)
+    @property
+    def teacher(self):
+        return self._teacher
+    @teacher.setter
+    def teacher(self,value):
+        if not isinstance(value,Teacher):
+            raise TypeError("teacher must be an instance of Teacher class")
+        self._teacher=value
+class Teacher:
+    def __init__(self,name):
+        self.name=name
+    def students(self):
+        return [student for student in Student.all if student.teacher == self]
+    def add_students(self,student):
+        if not isinstance(student,Student):
+            raise TypeError("student must be an instance of Student class")
+        student.teacher = self
+
+student=Student("Alice",20)
+teacher=Teacher("Bob")
+student.teacher=teacher  #either have this
+print(student.teacher.name)
+teacher.add_students(student)  #or this
+for student in teacher.students():
+    print(student.name,student.age)
